64e702eb6734883e8b71bcb70ac3a49ac80f3ab5
quarkusio <no-reply@quarkus.io>
2022-07-15 11:39:12 +0000

    Initial commit

---
+++ src/native-test/java/io/wybis/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.wybis;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
