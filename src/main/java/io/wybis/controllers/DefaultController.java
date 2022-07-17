8ce43eef510196b0e41a3d40a4280587277146ad
Blocktrust Eialarasu <arasu@agnitechnologies.com>
2022-07-17 22:17:32 +0530

    updating latest changes...

--- src/main/java/io/wybis/controllers/DefaultController.java
+++ src/main/java/io/wybis/controllers/DefaultController.java
@@ -1,5 +1,7 @@
 package io.wybis.controllers;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 import lombok.extern.slf4j.Slf4j;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -7,18 +9,47 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 @Slf4j
 @Path("/")
 public class DefaultController {
 
+    private final Template index;
+
+    @ConfigProperty(name = "app.name")
+    String appName;
+
+    public DefaultController(Template index) {
+        this.index = requireNonNull(index, "index template is required");
+    }
+
     @GET
-    @Path("hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive...";
+    @Path("index")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return index.data("appName", this.appName).data("appMode", io.quarkus.runtime.LaunchMode.current());
+    }
+
+    @GET
+    @Path("ping")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> ping() {
+        return Map.of("Ping", "Pong");
+    }
+
+    @GET
+    @Path("app-info")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> appInfo() {
+        Map<String, Object> appInfo = Map.of(
+                "mode", io.quarkus.runtime.LaunchMode.current(),
+                "name", appName
+        );
+        return appInfo;
     }
 
     @GET
@@ -43,6 +74,9 @@ public class DefaultController {
     @Path("toggle-log")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, Object> toggleLog() {
-         return Map.of("FileLogState", fileLogState, "GreyLogState", greyLogState);
+        Map<String, Object> map = new HashMap<>();
+        map.put("oldFileLogState", fileLogState);
+        map.put("oldGrayLogState", greyLogState);
+        return map;
     }
 }
